0625a9a34173cc912ade82eac2d5c77e6db13c03
Marcus-Vinicius-Oliveira <97204048+Marcus-Vinicius-Oliveira@users.noreply.github.com>
2022-09-04 22:57:09 -0300

    Add files via upload

---
+++ EstruturaEscolha/src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.text.AbstractDocument.BranchElement;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int codigoProduto = 2;
+
+        switch(codigoProduto){
+            case 1:
+
+                break;
+            
+            case 2:
+
+                break;
+            
+            
+            default:    
+
+        }
+    }
+}
