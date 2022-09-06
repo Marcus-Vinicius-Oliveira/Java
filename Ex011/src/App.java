44b5b96bb45bb1fd8ce145738f6a4987d9a5de79
Marcus-Vinicius-Oliveira <97204048+Marcus-Vinicius-Oliveira@users.noreply.github.com>
2022-09-06 20:50:43 -0300

    Add files via upload

--- Ex011/src/App.java
+++ Ex011/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner lScanner = new Scanner(System.in);
+
+        float pCusto, valorVenda, percentual;
+
+        System.out.println("Preço de custo do produto: ");
+        pCusto = lScanner.nextFloat();
+
+        System.out.println("digite o percentual a ser acrescido ao preço de custo: ");
+        percentual = lScanner.nextFloat();
+
+        valorVenda = (percentual / 100 * pCusto) + pCusto ;
+
+        System.out.println("O valor do preço de custo é de R$ " + pCusto + " e o valor da venda é de R$ " + valorVenda + " já que foi adicionado " + percentual + " por cento ao valor de custo");
+
+
+
+
     }
 }
